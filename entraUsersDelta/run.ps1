6fb4e40eda4f6fa8081d0d807549a29f6ba15c8a
noendscripting <miresnic@microsoft.com>
2024-08-02 14:44:04 -0400

    testing output

--- entraUsersDelta/run.ps1
+++ entraUsersDelta/run.ps1
@@ -2,4 +2,17 @@
        
 param($Timer)
 Connect-MgGraph -Identity -NoWelcome -Debug
-invoke-mgGraphRequest -Method GET -Uri 'https://graph.microsoft.com/v1.0/users/delta?$select=id,manager,displayName'
+
+#Connect-AzAccount -Identity -Environment 'AzureCloud' 
+
+#$AccessToken = ConvertTo-SecureString ((Get-AzAccessToken -ResourceUrl "https://graph.microsoft.com").Token) -AsPlainText
+
+#connect-mgGraph -AccessToken $AccessToken 
+
+$headers = @{ 
+
+'ConsistencyLevel'= 'Eventual' 
+
+
+} 
+invoke-mgGraphRequest -Method GET -Uri 'https://graph.microsoft.com/v1.0/users/delta?$select=id,manager,displayName' -Headers $headers -OutputType PSObject
--- requirements.psd1
+++ requirements.psd1
@@ -3,7 +3,7 @@
 #
 @{
     # For latest supported version, go to 'https://www.powershellgallery.com/packages/Az'. Uncomment the next line and replace the MAJOR_VERSION, e.g., 'Az' = '5.*'
-    # 'Az' = 'MAJOR_VERSION.*'
+    'Az.Accounts' = '3.*'
     'Microsoft.Graph.Authentication' = '2.*'
     'Microsoft.Graph.Groups' = '2.*'
     'Microsoft.Graph.Users' = '2.*'
